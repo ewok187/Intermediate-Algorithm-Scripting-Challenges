e9187a11d0e504ff9463b6c6a8a782f0c9531235
ewok187 <k.wessalowski@gmx.de>
2017-12-17 22:52:39 +0100

    made it simpler + comments

--- Search and Replace.js
+++ Search and Replace.js
@@ -1,10 +1,8 @@
 function myReplace(str, before, after) {
-  var newString = str.replace(before, after);
+  var newString = str.replace(before, after);                                        // create variable "newString" with before replaced by after
 
-  if (before === before.charAt(0).toUpperCase() + before.slice(1)) {
-    newString = str.replace(before, after.charAt(0).toUpperCase() + after.slice(1));
-  } else {
-    newString = str.replace(before, after);
+  if (before === before.charAt(0).toUpperCase() + before.slice(1)) {                 // if the first letter of before is in upperCase
+    newString = str.replace(before, after.charAt(0).toUpperCase() + after.slice(1)); // replace "before" with an "after" that has a first letter in uppercase too
   }
   return newString;
 }
