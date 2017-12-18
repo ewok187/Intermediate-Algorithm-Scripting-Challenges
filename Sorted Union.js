0b2f4a1ea052caed7b815d20e215410bec55c3e2
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-18 23:17:28 +0100

    comments

--- Sorted Union.js
+++ Sorted Union.js
@@ -1,11 +1,11 @@
 function uniteUnique(arr1, arr2, arr3) {
 
-  var placeHold = [];
+  var placeHold = [];                                     //creates empty array
 
-  for (var i = 0; i < arguments.length; i++) {
-    for (var j = 0; j < arguments[i].length; j++) {
-      if (placeHold.indexOf(arguments[i][j]) === -1) {
-        placeHold.push(arguments[i][j]);
+  for (var i = 0; i < arguments.length; i++) {            //loop through length of arguments
+    for (var j = 0; j < arguments[i].length; j++) {       //loop through current index value of arguments
+      if (placeHold.indexOf(arguments[i][j]) === -1) {    //check if array contains the value of arguments
+        placeHold.push(arguments[i][j]);                  //add it to the end of placeHold
       }
     }
   }
