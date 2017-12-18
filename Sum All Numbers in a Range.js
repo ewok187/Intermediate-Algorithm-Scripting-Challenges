3999f1af478415d5dbcf6f056ad8cc12a9b4eaf0
Konrad Wessalowski <k.wessalowski@gmx.de>
2017-12-18 22:49:03 +0100

    comments

--- Sum All Numbers in a Range.js
+++ Sum All Numbers in a Range.js
@@ -1,12 +1,12 @@
 function sumAll(arr) {
-  var min = Math.min.apply(null, arr);
-  var max = Math.max.apply(null, arr);
-  var result = [];
+  var min = Math.min.apply(null, arr);    // stores minimum num in arr
+  var max = Math.max.apply(null, arr);    // stores maximum num in arr
+  var result = [];                        // create empty array
 
   for (i = min; i <= max; i++) {
-    result.push(i);
+    result.push(i);                       // loop from min to max and push the numbers in the result array
   }
-  return result.reduce((a, x) => a + x);
+  return result.reduce((a, x) => a + x);  // add all numbers in result array 
 }
 
 sumAll([4, 1, 6, 7]);
